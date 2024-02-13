c6bb68aaa862e7ea88d0c8ee01158e1a8d8e79a4
Tanjava <tanja.pustjens@live.nl>
2024-02-13 11:16:23 +0000

    Change debug in settings.py

--- vaso/settings.py
+++ vaso/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-tanjava-vaso-l1ty87bqb7v.ws-eu108.gitpod.io']
 
