b7ac61d0a2ffe5e00e114ae4ebd1307343734c5c
Tanjava <tanja.pustjens@live.nl>
2024-02-13 11:18:35 +0000

    Add heroku to allowed hosts

--- vaso/settings.py
+++ vaso/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-tanjava-vaso-l1ty87bqb7v.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-tanjava-vaso-l1ty87bqb7v.ws-eu108.gitpod.io', 'vaso-a123ae0ee468.herokuapp.com']
 
 
 # Application definition
