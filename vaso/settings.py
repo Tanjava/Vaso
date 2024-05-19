4f5eceaa819505b8bb2f85a4b18d138da4172e2c
Tanjava <tanja.pustjens@live.nl>
2024-05-19 21:03:53 +0000

    Setup admin page

--- vaso/settings.py
+++ vaso/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['vaso-24-4786ebfc4c67.herokuapp.com', '8000-tanjava-vaso-ooarmf6s47t.ws-eu111.gitpod.io']
 
